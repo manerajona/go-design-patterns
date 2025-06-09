03b9be069a76dae0a8a61818271c27475c8adf69
Jonathan Manera <manerajona@gmail.com>
2025-06-09 09:56:15 +0200

    solid lsp

---
+++ solid/lsp.go
@@ -0,0 +1,59 @@
+package main
+
+type Parallelogram interface {
+	GetWidth() int
+	SetWidth(width int)
+	GetHeight() int
+	SetHeight(height int)
+}
+
+type Rectangle struct {
+	width, height int
+}
+
+func (r *Rectangle) GetWidth() int {
+	return r.width
+}
+
+func (r *Rectangle) SetWidth(width int) {
+	r.width = width
+}
+
+func (r *Rectangle) GetHeight() int {
+	return r.height
+}
+
+func (r *Rectangle) SetHeight(height int) {
+	r.height = height
+}
+
+type NaiveSquare struct {
+	Rectangle
+}
+
+func NewNaiveSquare(size int) *NaiveSquare {
+	sq := NaiveSquare{}
+	sq.width = size
+	sq.height = size
+	return &sq
+}
+
+// breaks liskov substitution principle
+func (s *NaiveSquare) SetWidth(width int) {
+	s.width = width
+	s.height = width
+}
+
+// breaks liskov substitution principle
+func (s *NaiveSquare) SetHeight(height int) {
+	s.width = height
+	s.height = height
+}
+
+type Square struct {
+	size int
+}
+
+func (s *Square) Rectangle() *Rectangle {
+	return &Rectangle{s.size, s.size}
+}
---
+++ solid/lsp_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func testBehaviour(t *testing.T, p Parallelogram, newHeight int) {
+	expectedArea := newHeight * p.GetWidth()
+	p.SetHeight(newHeight)
+	actualArea := p.GetWidth() * p.GetHeight()
+	if actualArea != expectedArea {
+		t.Errorf("Expected area %d, got %d", expectedArea, actualArea)
+	}
+}
+
+func TestRectangleBehaviour_pass(t *testing.T) {
+	rectangle := &Rectangle{2, 3}
+	testBehaviour(t, rectangle, 10)
+}
+
+func TestNaiveSquareBehaviour_fail(t *testing.T) {
+	square := NewNaiveSquare(5)
+	testBehaviour(t, square, 10)
+}
+
+func TestSquareBehaviour_pass(t *testing.T) {
+	square := &Square{size: 5}
+	testBehaviour(t, square.Rectangle(), 10)
+}
